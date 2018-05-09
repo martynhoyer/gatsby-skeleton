5a0c9092c086793d090e95f805ac017472188a03
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-09 17:37:56 +0100

    Update pagination to reflect design
    - Pull apart the `prev` and `next` items to build out the rest

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -4,29 +4,54 @@ import PaginationLink from "../PaginationLink";
 import spacing from "../../tokens/dimensions";
 
 const Container = styled.nav`
-  display: grid;
-  grid-gap: ${spacing.md};
-  grid-template-columns: 1fr auto 1fr;
+  display: flex;
+  justify-content: center;
 
   margin-top: ${spacing.xxl};
   text-align: center;
   color: ${props => props.theme.palette.noir};
 `;
 
+const CurrentPage = styled.span`
+  width: ${spacing.lg};
+  height: ${spacing.lg};
+  margin: ${spacing.sm};
+  padding: ${spacing.sm};
+  line-height: 1;
+  border-radius: 50%;
+  background-color: ${props => props.theme.palette.rose};
+  color: ${props => props.theme.palette.blanc};
+`;
+
 class Pagination extends React.Component {
   render() {
     const { page, pages, prev, next } = this.props;
+
+    const getUrlRoot = string => {
+      const arrayFromString = string.split("/");
+      arrayFromString.pop();
+      const urlRoot = arrayFromString.join("/");
+      return urlRoot;
+    };
+
+    const urlRoot = getUrlRoot(next || prev);
+    const nextAsNumber = next && Number(next.split("/").pop());
+    const prevAsNumber = prev && Number(prev.split("/").pop());
+    const nextPlusOneUrl =
+      nextAsNumber < pages && `${urlRoot}/${nextAsNumber + 1}`;
+    const prevMinusOneUrl =
+      prevAsNumber > 1 &&
+      `${urlRoot}/${prevAsNumber - 1 === 1 ? "" : prevAsNumber - 1}`;
+
     return (
       <Container>
-        <div>
-          <PaginationLink url={prev} prev text="Newer posts" />
-        </div>
-        <span>
-          Page {page} of {pages}
-        </span>
-        <div>
-          <PaginationLink url={next} text="Older posts" />
-        </div>
+        <PaginationLink url={prev} prev text="&larr;" />
+        <PaginationLink url={prevMinusOneUrl} text={page - 2} />
+        <PaginationLink url={prev} prev text={page - 1} />
+        <CurrentPage>{page}</CurrentPage>
+        <PaginationLink url={next} text={page + 1} />
+        <PaginationLink url={nextPlusOneUrl} text={page + 2} />
+        <PaginationLink url={next} text="&rarr;" />
       </Container>
     );
   }
--- src/components/PaginationLink/index.jsx
+++ src/components/PaginationLink/index.jsx
@@ -1,43 +1,23 @@
 import React from "react";
 import Link from "gatsby-link";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import spacing from "../../tokens/dimensions";
 
-const arrowDirection = ({ prev }) =>
-  prev
-    ? css`
-        &::before {
-          content: "\u2190";
-          margin-right: ${spacing.md};
-        }
-      `
-    : css`
-        &::after {
-          content: "\u2192";
-          margin-left: ${spacing.md};
-        }
-      `;
-
 const StyledLink = styled(Link)`
-  ${arrowDirection};
-
+  width: ${spacing.lg};
+  height: ${spacing.lg};
+  margin: ${spacing.sm};
+  padding: ${spacing.sm};
+  line-height: 1;
+  border-radius: 50%;
   text-decoration: none;
   color: ${props => props.theme.palette.noir};
-
-  &::before,
-  &::after {
-    color: ${props => props.theme.palette.rose};
-  }
 `;
 
 class PaginationLink extends React.Component {
   render() {
     if (this.props.url) {
-      return (
-        <StyledLink prev={this.props.prev} to={this.props.url}>
-          {this.props.text}
-        </StyledLink>
-      );
+      return <StyledLink to={this.props.url}>{this.props.text}</StyledLink>;
     }
     return null;
   }
