99d5f74eb9b84f81bd9c9de817f28a00b531cf95
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-11 22:00:59 +0100

    Add tag icon

--- src/components/PostTags/index.jsx
+++ src/components/PostTags/index.jsx
@@ -3,6 +3,7 @@ import _ from "lodash";
 import Link from "gatsby-link";
 import { FormattedMessage, injectIntl } from "react-intl";
 import styled from "styled-components";
+import { ReactComponent as TagIcon } from "../../svg/tag.svg";
 import spacing, { fontsize } from "../../tokens/dimensions";
 
 const Container = styled.div`
@@ -14,6 +15,14 @@ const Heading = styled.h2`
   margin: 0 0 ${spacing.base};
   font-size: ${fontsize.md};
 `;
+
+const StyledTagIcon = styled(TagIcon)`
+  width: 1em;
+  height: 1em;
+  margin-right: 0.5em;
+  vertical-align: middle;
+  fill: ${props => props.theme.palette.grisLight};
+`
 
 const StyledLink = styled(Link)`
   text-decoration: none;
@@ -28,7 +37,7 @@ class PostTags extends Component {
       return (
         <Container>
           <Heading>
-            <FormattedMessage id="post.tagsHeading" />
+            <StyledTagIcon /><FormattedMessage id="post.tagsHeading" />
           </Heading>
           {tags.map((tag, index, arr) => (
             <span key={tag}>
