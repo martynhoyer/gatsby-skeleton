5866e1a4c91df1a2266856ed7815fdb27c035aee
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-12 13:13:54 +0100

    Fix category title on related posts query

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -264,6 +264,7 @@ export const pageQuery = graphql`
             }
           }
           categoriesArray: childrenCategoriesJson {
+            title
             displayName
             color
           }
