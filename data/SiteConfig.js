cb9f5fbd354ec24cc84f3a63f754a9cfd056e3cd
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-14 22:58:39 +0100

    Update global config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,11 +2,10 @@ module.exports = {
   blogPostDir: "posts", // The name of directory that contains your posts.
   blogAuthorDir: "authors", // The name of directory that contains your 'authors' folder.
   blogAuthorId: "martyn", // The default and fallback author ID used for blog posts without a defined author.
-  siteTitle: "Gatsby blog", // Site title.
-  siteUrl: "https://martynhoyer.co.uk",
+  siteTitle: "Gymlib blog", // Site title.
+  siteUrl: "https://blog.gymlib.com",
   siteNavigation: true, // If navigation is enabled the Menu button will be visible
   sitePaginationLimit: 6, // The max number of posts per page.
-  googleAnalyticsID: "", // GA tracking ID.
   locales: ["fr", "en"],
   defaultLangKey: "fr",
   // Copyright string for the footer of the website and RSS feed.
