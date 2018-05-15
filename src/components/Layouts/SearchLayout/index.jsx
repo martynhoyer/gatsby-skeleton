3fbb408a31699f04fb7003cd1526cf81cf16beab
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-15 08:29:17 +0100

    Add search page layout

---
+++ src/components/Layouts/SearchLayout/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+import media from "../../../tokens/breakpoints";
+import spacing from "../../../tokens/dimensions";
+
+const StyledSearchLayout = styled.div`
+  display: grid;
+  grid-gap: ${spacing.md};
+
+  margin-right: auto;
+  margin-left: auto;
+  padding: ${spacing.md} ${spacing.md} ${spacing.xxl};
+
+  @media (${media.md}) {
+    grid-template-columns: 1fr 5fr 1fr;
+
+    max-width: 1200px;
+  }
+`;
+
+export const SearchResults = styled.div`
+  @media (${media.md}) {
+    grid-column-start: 2;
+  }
+`;
+
+export const SearchLayout = ({ children }) => (
+  <StyledSearchLayout>{children}</StyledSearchLayout>
+);
