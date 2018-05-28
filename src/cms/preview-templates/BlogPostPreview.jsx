223b97bd5dc461876be0d841fd7d0b89483524bc
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-28 12:52:16 +0100

    CMS post preview date formatting

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Container = styled.div`
-  outline: 10px solid red;
-`
+import moment from 'moment'
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
+  const locale = entry.getIn(['data', 'locale'])
+  require(`moment/locale/${locale}`)
+  const date = moment(new Date(entry.getIn(['data', 'date'])))
+    .format('YYYY-MM-DD')
+    .locale()
   return (
-    <Container>
+    <div>
+      <div>{entry.getIn(['data', 'image'])}</div>
       <div>{entry.getIn(['data', 'category'])}</div>
-      <div>{entry.getIn(['data', 'date'])}</div>
-      <div>{entry.getIn(['data', 'title'])}</div>
+      <div>{date}</div>
+      <h1>{entry.getIn(['data', 'title'])}</h1>
       <div>{widgetFor('body')}</div>
       <div>{entry.getIn(['data', 'tags'])}</div>
-    </Container>
+    </div>
   )
 }
 
