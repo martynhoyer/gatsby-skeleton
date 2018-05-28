e83265eace2635b9d3ebe786065d99a52c0b2c1b
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-28 12:29:43 +0100

    Add data to blog post CMS preview and test styles

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  outline: 10px solid red;
+`
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  console.log(entry);
   return (
-    <div>
-      <div>{entry.getIn(["data", "title"])}</div>
-      <div>{widgetFor("body")}</div>
-      <div>{entry.getIn(["data", "description"])}</div>
-      <div>{entry.getIn(["data", "tags"])}</div>
-    </div>
-  );
-};
+    <Container>
+      <div>{entry.getIn(['data', 'category'])}</div>
+      <div>{entry.getIn(['data', 'date'])}</div>
+      <div>{entry.getIn(['data', 'title'])}</div>
+      <div>{widgetFor('body')}</div>
+      <div>{entry.getIn(['data', 'tags'])}</div>
+    </Container>
+  )
+}
 
-export default BlogPostPreview;
+export default BlogPostPreview
