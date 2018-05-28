f3894e264d86a3bc580c56648aa269eebf7a6449
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-28 13:48:29 +0100

    Add category color and sans-serif font to CMS post preview styles

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -10,19 +10,23 @@ const BlogPostPreview = ({ entry, widgetFor }) => {
   const authorJsonPath = `${entry.getIn(['data', 'author'])}`
   const author = require(`../../../content/authors/${authorJsonPath}.json`)
   return (
-    <div>
+    <div
+      style={{
+        fontFamily:
+          '-apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, “Fira Sans”, “Droid Sans”, “Helvetica Neue”, Arial, sans-serif',
+      }}>
       <img style={{ display: 'block', maxWidth: '100%' }} src={entry.getIn(['data', 'image'])} alt="" />
       <div style={{ textAlign: 'center', marginTop: '1em' }}>
-        {category.displayName} {date}
+        <span style={{ color: category.color }}>{category.displayName}</span> {date}
       </div>
       <h1>{entry.getIn(['data', 'title'])}</h1>
       <div>{widgetFor('body')}</div>
       <div style={{ textAlign: 'center' }}>
-        <h4 style={{ marginBottom: '0' }}>Tags</h4>
+        <h4 style={{ marginBottom: '0.5em' }}>Tags</h4>
         {entry.getIn(['data', 'tags'])}
       </div>
       <div style={{ textAlign: 'center', marginTop: '1em' }}>
-        <h4>Author</h4>
+        <h4 style={{ marginBottom: '0.5em' }}>Author</h4>
         {author.displayName}
       </div>
     </div>
