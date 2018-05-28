3ce917baecf3439226fd1985a1a7381123e4dd84
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-28 13:30:04 +0100

    Post preview formatting/styles

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -6,19 +6,20 @@ const BlogPostPreview = ({ entry, widgetFor }) => {
   require(`moment/locale/${locale}`)
   const date = moment(new Date(entry.getIn(['data', 'date']))).format('DD MMMM YYYY')
   const categoryJsonPath = `${locale}-${entry.getIn(['data', 'category'])}`
-  console.log(categoryJsonPath)
   const category = require(`../../../content/categories/${categoryJsonPath}.json`)
-  console.log(category.displayName)
   return (
     <div>
       <div>
-        <img src={entry.getIn(['data', 'image'])} alt="" />
+        <img style={{ maxWidth: '100%' }} src={entry.getIn(['data', 'image'])} alt="" />
       </div>
-      <div>{category.displayName}</div>
-      <div>{date}</div>
+      <div>
+        {category.displayName} {date}
+      </div>
       <h1>{entry.getIn(['data', 'title'])}</h1>
       <div>{widgetFor('body')}</div>
-      <div>{entry.getIn(['data', 'tags'])}</div>
+      <div>
+        <h4>Tags</h4> {entry.getIn(['data', 'tags'])}
+      </div>
     </div>
   )
 }
