aadb8cc41c4e334e37d9fcba8025394863332552
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-28 13:09:56 +0100

    Add category to CMS  post preview

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -5,10 +5,14 @@ const BlogPostPreview = ({ entry, widgetFor }) => {
   const locale = entry.getIn(['data', 'locale'])
   require(`moment/locale/${locale}`)
   const date = moment(new Date(entry.getIn(['data', 'date']))).format('DD MMMM YYYY')
+  const category = require(`../../../content/categories/${locale}-${entry.getIn(['data', 'category'])}`)
+  console.log(category.displayName)
   return (
     <div>
-      <div>{entry.getIn(['data', 'image'])}</div>
-      <div>{entry.getIn(['data', 'category'])}</div>
+      <div>
+        <img src={entry.getIn(['data', 'image'])} alt="" />
+      </div>
+      <div>{category.displayName}</div>
       <div>{date}</div>
       <h1>{entry.getIn(['data', 'title'])}</h1>
       <div>{widgetFor('body')}</div>
