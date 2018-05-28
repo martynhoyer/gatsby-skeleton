723a0a0d052c8d787f8043ad818eec4b8a996627
Martyn Hoyer <martyn@corseconcierge.com>
2018-05-28 13:39:45 +0100

    Add author to CMS post preview

--- src/cms/preview-templates/BlogPostPreview.jsx
+++ src/cms/preview-templates/BlogPostPreview.jsx
@@ -7,18 +7,23 @@ const BlogPostPreview = ({ entry, widgetFor }) => {
   const date = moment(new Date(entry.getIn(['data', 'date']))).format('DD MMMM YYYY')
   const categoryJsonPath = `${locale}-${entry.getIn(['data', 'category'])}`
   const category = require(`../../../content/categories/${categoryJsonPath}.json`)
+  const authorJsonPath = `${entry.getIn(['data', 'author'])}`
+  const author = require(`../../../content/authors/${authorJsonPath}.json`)
   return (
     <div>
-      <div>
-        <img style={{ maxWidth: '100%' }} src={entry.getIn(['data', 'image'])} alt="" />
-      </div>
-      <div>
+      <img style={{ display: 'block', maxWidth: '100%' }} src={entry.getIn(['data', 'image'])} alt="" />
+      <div style={{ textAlign: 'center', marginTop: '1em' }}>
         {category.displayName} {date}
       </div>
       <h1>{entry.getIn(['data', 'title'])}</h1>
       <div>{widgetFor('body')}</div>
-      <div>
-        <h4>Tags</h4> {entry.getIn(['data', 'tags'])}
+      <div style={{ textAlign: 'center' }}>
+        <h4 style={{ marginBottom: '0' }}>Tags</h4>
+        {entry.getIn(['data', 'tags'])}
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '1em' }}>
+        <h4>Author</h4>
+        {author.displayName}
       </div>
     </div>
   )
