97abd1cfe5775414e063ea1ad38bd61fbf417a6c
Sebastien Fichot <fichot.sebastien@gmail.com>
2018-06-01 12:46:44 +0200

    feat(comments): Comments on cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,7 @@
 import CMS from 'netlify-cms'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 
+// Registers the English preview template
 CMS.registerPreviewTemplate('english_posts', BlogPostPreview)
+// Registers the French preview template
 CMS.registerPreviewTemplate('french_posts', BlogPostPreview)
